adfcb1cd47b018562e67ac0bd9827204d6484a27
AcuaRi <81779747+AcuaRi@users.noreply.github.com>
2025-02-06 21:18:15 +0900

    Add Enemy Move&Attack logic

--- Assets/Scripts/Enemy/GeneralEnemy.cs
+++ Assets/Scripts/Enemy/GeneralEnemy.cs
@@ -34,7 +34,7 @@ public class GeneralEnemy : MonoBehaviour, IDamageable
 
         if (animator == null)
         {
-            animator = GetComponent<Animator>();
+            //animator = GetComponent<Animator>();
         }
         
         if (sprite == null)
@@ -47,7 +47,7 @@ public class GeneralEnemy : MonoBehaviour, IDamageable
         
         //null check
         if(rb == null) {Debug.LogError($"{this.gameObject.name}(RigidBody2D) is null");}
-        if(animator == null) {Debug.LogError($"{this.gameObject.name}(Animator) is null");}
+        //if(animator == null) {Debug.LogError($"{this.gameObject.name}(Animator) is null");}
         if(sprite == null) {Debug.LogError($"{this.gameObject.name}(Sprite) is null");}
         if(refData == null) {Debug.LogError($"{this.gameObject.name}(refData) is null");}
         //if( generalMonsterData.targetLayer != PlayerLayer) {Debug.LogError($"{this.gameObject.name}(targetLayer is not playerLayer)");}
@@ -110,7 +110,8 @@ public class GeneralEnemy : MonoBehaviour, IDamageable
 
     protected virtual void IdleEnter()
     {
-        Invoke("CheckTarget", 1f);
+        generalMonsterData.moveDirection = ( Vector2.right * Random.Range(-1f, 1f) + Vector2.down * Random.Range(0.5f, 1f)).normalized;
+        Invoke("CheckTarget", 0.25f);
     }
     
     protected virtual void IdleUpdate()
@@ -120,12 +121,19 @@ public class GeneralEnemy : MonoBehaviour, IDamageable
     
     protected virtual void AttackEnter()
     {
-        animator.SetTrigger("Attack");
+        //animator.SetTrigger("Attack");
         Attack();
     }
     
     protected virtual void AttackUpdate()
     {
+        if (generalMonsterData.targetTransform == null)
+        {
+            nextState = idleState;
+            return;
+        }
+        generalMonsterData.moveDirection = (generalMonsterData.targetTransform.position - transform.position).normalized;
+        rb.transform.Translate( generalMonsterData.moveSpeed * Time.deltaTime *  generalMonsterData.moveDirection);
         sprite.flipX = ( generalMonsterData.targetTransform.position.x < transform.position.x);
     }
     /// 
@@ -173,7 +181,7 @@ public class GeneralEnemy : MonoBehaviour, IDamageable
     
     protected virtual void OnCollisionEnter2D(Collision2D other)
     {
-        // if ( currentState == deathState) return;
+        if ( currentState == deathState) return;
         //
         // if (other.gameObject.layer == 9)
         // {
@@ -185,11 +193,44 @@ public class GeneralEnemy : MonoBehaviour, IDamageable
         //     other.gameObject.GetComponent<PlayerController>().GetDamaged( generalMonsterData.attackDamage, this.gameObject,
         //         (((other.transform.position.x > transform.position.x) ? Vector2.right : Vector2.left) + 0.5f * Vector2.up).normalized *  generalMonsterData.knockBackPower);
         // }
+        
+        if (other.gameObject.layer == Mathf.Log(generalMonsterData.targetLayer.value, 2))
+        {
+            //Debug.Log($"{this.gameObject.name}({other.gameObject.name}) is detected");
+            //Destroy(other.gameObject);
+        }
+        else
+        {
+            TurnBack();
+        }
+    }
+
+    private float damageTimer = 0f;
+    protected virtual void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.layer == Mathf.Log(generalMonsterData.targetLayer.value, 2))
+        {
+            damageTimer += Time.deltaTime;
+
+            // 누적된 시간이 1초 이상이면 데미지를 적용
+            if (damageTimer >= 1f)
+            {
+                // 충돌한 대상의 Cell 컴포넌트를 가져와 데미지를 입힘
+                var cell = other.gameObject.GetComponent<Cell>();
+                if (cell != null)
+                {
+                    cell.GetDamaged(generalMonsterData.attackDamage);
+                }
+
+                // 타이머 초기화
+                damageTimer = 0f;
+            }
+        }
     }
 
     protected virtual void Attack()
     {
-        Debug.Log("Attack!");
+        //Debug.Log("Attack!");
     }
     
     public void GetDamaged(float damage, MedicineType medicineType)
