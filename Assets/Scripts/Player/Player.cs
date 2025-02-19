20fc54b18aff5cd7b5bd8039ce5addb4ce4bee62
tarakoOpO <hiyasinsuyurusite@gmail.com>
2025-02-19 18:47:30 +0900

    add shooting type
    攻撃の種類を少し増やしました。

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -14,9 +14,7 @@ public class Player : MonoBehaviour, IDamageable
     private Rigidbody2D _rb;
     private Vector2 _input;
 
-    public GameObject bulletPrefab;
-    /*[SerializeField]
-    private int poolSize = 20;*/
+    public GameObject bullet;
     private Vector3 bulletPoint;
     
     //Stop Player for 10secs when hp < 0
@@ -29,10 +27,30 @@ public class Player : MonoBehaviour, IDamageable
     private Color _originalColor;
     
     private HPGauge hpGaugeInstance;
+    private bool _isShooting;
+    private Coroutine _shootingCoroutine;
 
-    // private Queue<GameObject> bulletPool = new Queue<GameObject>();
+    [SerializeField]
+    private MedicineType _medicineType = MedicineType.Medicine1; // �����l
+    private int _medicineNum = 1;
+    // �������牺�͒e���ꂼ��̏��A�f�[�^�V�X�e���I�Ȃ̂ɑg�ݍ��ނ���
+    [SerializeField]
+    private GameObject[] bulletPrefabs = new GameObject[10];
+    [SerializeField]
+    private float[] _firingrate = new float[10];
+    [SerializeField]
+    private ShootingMode[] shootingModes = new ShootingMode[10];
 
-    // Start is called before the first frame update
+    private ShootingMode _currentMode = ShootingMode.Single; // �����l��Single�ɐݒ�
+
+    // �ˌ����[�h�̗񋓌^�A������������Ȃ���������
+    public enum ShootingMode
+    {
+        Single,       // 1���� (type0)
+        Triple,       // 3���� (type1)
+        Omni         // 8���� (type2)
+    }
+
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -41,12 +59,7 @@ public class Player : MonoBehaviour, IDamageable
         _sr = GetComponentInChildren<SpriteRenderer>();
         _originalColor = _sr.color;
         _currentHP = _playerHP;
-        /*for (int i = 0; i < poolSize; i++)
-        {
-            GameObject bullet = Instantiate(bulletPrefab);
-            bullet.SetActive(false);
-            bulletPool.Enqueue(bullet);
-        }*/
+        _isShooting = false;
     }
 
     private void Start()
@@ -66,6 +79,24 @@ public class Player : MonoBehaviour, IDamageable
     {
         if (!isAlive) return;
         _Move();
+        _medicineChoice();
+        // Debug.Log(_isCoroutine);
+
+        if (_isShooting && _shootingCoroutine == null)
+        {
+            switch (_currentMode)
+            {
+                case ShootingMode.Single:
+                    _shootingCoroutine = StartCoroutine(shootingType0());
+                    break;
+                case ShootingMode.Triple:
+                    _shootingCoroutine = StartCoroutine(shootingType1());
+                    break;
+                case ShootingMode.Omni:
+                    _shootingCoroutine = StartCoroutine(shootingType2());
+                    break;
+            }
+        }
     }
 
     public void _OnMove(InputAction.CallbackContext context)
@@ -78,9 +109,30 @@ public class Player : MonoBehaviour, IDamageable
         if (context.performed)
         {
             if(!isAlive) return;
-            
-            GameObject b = Instantiate(bulletPrefab, transform.position + bulletPoint, Quaternion.identity);
-            b.GetComponent<Bullet>().getVector(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            _isShooting = true;
+            /*GameObject b = Instantiate(bullet, transform.position + bulletPoint, Quaternion.identity);
+            b.GetComponent<Bullet>().getVector(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));*/
+        }
+    }
+
+    public void _OnRelease(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            _isShooting= false;
+            if (_shootingCoroutine != null) // �R���[�`���������Ă�����~�߂�
+            {
+                StopCoroutine(_shootingCoroutine);
+                _shootingCoroutine = null;
+            }
+        }
+    }
+
+    public void _OnBoth(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            // �����ǂ�����?
         }
     }
 
@@ -89,7 +141,95 @@ public class Player : MonoBehaviour, IDamageable
         _rb.velocity = new Vector2(_input.x * _playerSpeed, _input.y * _playerSpeed);
     }
 
-    public float GetPlayerHP()
+    private void _medicineChoice() // Player�N���X���ł��Ȃ��Ă����������A�e��type�������Ă��Ȃ��ƃ_���[�W���荢��̂Œe�ɂ��^�C�v����
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            _medicineType = MedicineType.Medicine1;
+            _medicineNum = 1;
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            _medicineType = MedicineType.Medicine2;
+            _medicineNum = 2;
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            _medicineType = MedicineType.Medicine3;
+            _medicineNum = 3;
+        }
+        else if (Input.GetKey(KeyCode.Alpha4))
+        {
+            _medicineType = MedicineType.Medicine4;
+            _medicineNum = 4;
+        }
+        bullet = bulletPrefabs[_medicineNum - 1];
+        _currentMode = shootingModes[_medicineNum - 1];
+    }
+
+    IEnumerator shootingType0()
+    {
+        while (_isShooting)
+        {
+            GameObject b = Instantiate(bullet, transform.position + bulletPoint, Quaternion.identity);
+            b.GetComponent<Bullet>().getVector(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            yield return new WaitForSeconds(_firingrate[_medicineNum - 1]);
+        }
+        _shootingCoroutine = null;
+    }
+
+    IEnumerator shootingType1()
+    {
+        while (_isShooting)
+        {
+            // �v���C���[����}�E�X�ւ̕������擾�i���K���j
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0f; // 2D�Q�[���̏ꍇ�AZ���͖���
+            Vector3 shootDirection = (mousePos - transform.position).normalized;
+
+            // ���ˊp�x�����߂�i�v���C���[���猩���}�E�X�̕����j
+            float baseAngle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
+
+            // 3�����ɔ��ˁi-30�x, 0�x, +30�x�j
+            float[] angles = { -30f, 0f, 30f };
+
+            foreach (float angleOffset in angles)
+            {
+                float finalAngle = baseAngle + angleOffset;
+                Vector3 bulletDirection = new Vector3(Mathf.Cos(finalAngle * Mathf.Deg2Rad), Mathf.Sin(finalAngle * Mathf.Deg2Rad), 0f);
+
+                GameObject b = Instantiate(bullet, transform.position + bulletPoint, Quaternion.identity);
+                b.GetComponent<Bullet>().getVector(transform.position, transform.position + bulletDirection);
+            }
+
+            yield return new WaitForSeconds(_firingrate[_medicineNum - 1]);
+        }
+        _shootingCoroutine = null;
+    }
+
+    IEnumerator shootingType2()
+    {
+        while (_isShooting)
+        {
+            // 8�����̔��ˊp�x�i0�x, 45�x, 90�x, ..., 315�x�j
+            float[] angles = { 0f, 45f, 90f, 135f, 180f, 225f, 270f, 315f };
+
+            foreach (float angle in angles)
+            {
+                // �p�x���x�N�g���ɕϊ�
+                Vector3 bulletDirection = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0f);
+
+                // �e�𐶐����A������ݒ�
+                GameObject b = Instantiate(bullet, transform.position + bulletPoint, Quaternion.identity);
+                b.GetComponent<Bullet>().getVector(transform.position, transform.position + bulletDirection);
+            }
+
+            yield return new WaitForSeconds(_firingrate[_medicineNum - 1]);
+        }
+        _shootingCoroutine = null;
+    }
+
+    public int GetPlayerHP()
     {
         return _playerHP;
     }
